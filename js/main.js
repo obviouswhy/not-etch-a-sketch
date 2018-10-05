b0c59e8f4ca0bbe79a1b14a968d9b4db2e8e9557
obviouswhy <brew@localhost.localdomain>
2018-10-05 20:23:44 +0900

    etch sketch

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
-var a = document.querySelector(".sketchContainer");
+var container = document.querySelector(".sketchContainer");
 var div = document.createElement('div');
 div.classList.add('square');
+
 for (var i = 1; i <= 256; i++){
-  a.appendChild(div.cloneNode());
+  container.appendChild(div.cloneNode());
   
 }
+
 var sqs = document.querySelectorAll(".square");
 sqs.forEach(sqr=>{
-    /*sqr.addEventListener('click', (e) => {
-        sqr.classList.toggle('black');
-    });*/
+    sqr.addEventListener('click', (e) => {
+        sqr.classList.toggle('painted');
+    });
     sqr.addEventListener('mouseover', (e) => {
         if (e.buttons == 1) {
-            sqr.classList.add('black');
+            sqr.classList.add('painted');
         }
     });
-})
+});
+
+var resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener('click', (e) => resetCells());
+
+function resetCells() {
+    sqs.forEach(sqr => sqr.classList.remove('painted'));
+}
+
+var chooseColor = document.querySelector(".colorPick"); /*This one is the button that will activte the input type color*/ 
+var colorDisplay = document.querySelector(".colorPick > i");
+var colorPicker = document.querySelector("#colorPicker");; /*This one is the input type color*/ 
+var mainColor = "black";
+colorPicker.addEventListener("input", (e) => {
+    mainColor = document.querySelector("#colorPicker").value;
+    colorDisplay.setAttribute("style","color:" + mainColor);
+});
+
+chooseColor.addEventListener('click', (e) =>colorPicker.click());
