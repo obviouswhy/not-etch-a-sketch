1038053446404e22a1cedb04bf9df3b686446233
obviouswhy <brew@localhost.localdomain>
2018-10-05 15:24:48 +0900

    etch sketch

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var a = document.querySelector(".sketchContainer");
+var div = document.createElement('div');
+div.classList.add('square');
+for (var i = 1; i <= 256; i++){
+  a.appendChild(div.cloneNode());
+  
+}
+var sqs = document.querySelectorAll(".square");
+sqs.forEach(sqr=>{sqr.addEventListener('click', (e) => {
+    
+    sqr.classList.toggle('black');
+  });});
