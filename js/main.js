5da0685135e37d9649f8698bc3160b2d3dd10435
obviouswhy <brew@localhost.localdomain>
2018-10-08 15:40:03 +0900

    fixing color picker in safari

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 
+/*  popukate the div sketchContainer with the cells to be painted*/
 var container = document.querySelector(".sketchContainer");
 var div = document.createElement('div');
 div.classList.add('square');
@@ -8,28 +9,37 @@ for (var i = 1; i <= 256; i++){
   container.appendChild(div.cloneNode());
   
 }
-
+/* 
+- sets te eventListeners for click and drag
+- checks if the cell is painted, if painted -> remove color, else paint 
+*/
 var sqs = document.querySelectorAll(".square");
 sqs.forEach(sqr=>{
     sqr.addEventListener('click', (e) => {
         sqr.classList.toggle('painted');
-        //sqr.removeAttribute("style", "background-color:"+mainColor);
+        if (sqr.classList.contains('painted')) {
+            sqr.setAttribute("style", "background-color:"+mainColor);  
+        }else{
+            sqr.setAttribute("style", "background-color: inherit");
+        }
     });
     sqr.addEventListener('mouseover', (e) => {
         if (e.buttons == 1) {
             sqr.classList.add('painted');
-            //sqr.setAttribute("style", "background-color:"+mainColor);
+            sqr.setAttribute("style", "background-color:"+mainColor);
         }
     });
 });
 
+/* when the reset button is pressed resets the color of all the cells */
 var resetBtn = document.querySelector(".reset-btn");
 resetBtn.addEventListener('click', (e) => resetCells());
 
 function resetCells() {
-    sqs.forEach(sqr => sqr.classList.remove('painted'));
+    //sqs.forEach(sqr => sqr.classList.remove('painted'));
+        sqs.forEach(sqr => sqr.setAttribute('style', 'background-color:inherit'));
 }
-
+/* gets the value from the color picker to set the main color and change the color of the square inside the button */
 var mainColor = "black";
 var colorDisplay = document.querySelector(".colorPick > i");
     function updateColor(col) {
